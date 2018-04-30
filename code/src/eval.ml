7aa1008671b80a2c25f29e9afa7e12f2ffd0257d
Xyouz <marius.dufraisse@gmail.com>
2018-04-30 22:36:14 +0200

    Début d'eval

--- code/src/eval.ml
+++ code/src/eval.ml
@@ -1,3 +1,51 @@
 open Ast
 
+let rec subs exp x rep = match exp with
+  | l,Var(y) when x=y -> rep
+  | _,Var(_)| _,Int(_) |_,Bool(_) -> exp
+  | l,App(u,t) -> l,App((subs u x rep), (subs t x rep))
+  | l,Lam(y,_,e) -> (
+      if (x = y) then
+        failwith "Bound variable when subs in lambda"
+      else
+        l,Lam(y,None,(subs e x rep))
+    )
+  | l,Pair(e1,e2) -> let e1p = subs e1 x rep and
+    e2p = subs e2 x rep in
+    l,Pair(e1p,e2p)
+  | l,Ite(c,e1,e2) -> let cp = subs c x rep and
+    e1p = subs e1 x rep and
+    e2p = subs e2 x rep in
+    l,Ite(cp, e1p, e2p)
+  | l,Fix(e) -> l,Fix((subs e x rep))
+  | l,Binop(op,e1,e2) -> let e1p = subs e1 x rep and
+    e2p = subs e2 x rep in
+    l,Binop(op, e1p,e2p)
+  | l,Proj(Left(e)) -> l,Proj(Left(subs e x rep))
+  | l,Proj(Right(e)) -> l,Proj(Right(subs e x rep))
+  | l,LetIn(y,e1,e2) -> (
+      if (x = y) then
+        failwith "Bound variable when subs in LetIn"
+      else
+        let e1p = subs e1 x rep and
+        e2p = subs e2 x rep in
+        l,LetIn(y,e1p,e2p)
+    )
+ 
+let isValue exp = match exp with
+  | _,Lam(_,_,_) | _,Pair(_,_) | _,Int(_) | _,Bool(_) -> true
+  | _ -> false
+
+let rec eval_expr expr context = match expr with
+  | _, Var(v) -> List.assoc v context
+  | l, App((ll,Lam(x,_,t)),u) -> (
+      if (isValue u) then
+         eval_expr (subs t x u) context
+      else
+        let up = eval_expr u context in 
+        eval_expr (l,App((ll,Lam(x,None,t)), up)) context 
+    )
+  | l,App(t,u) -> let tp = eval_expr t context in (l,App(tp,u))
+  | _ -> failwith "TODO eval expr"
+
 let eval_ast cmds = failwith "todo eval"
--- code/src/typing.ml
+++ code/src/typing.ml
@@ -1,28 +1,43 @@
 open Ast
+open Utils
 
 type typing_error = int
 
-exception Typing_Error
+exception Typing_Error of string
 
 let print_error fmt err = failwith "todo print error"
 
 
 let rec typing_expr expr context = match expr with
-  | _,Var(s) -> failwith "todo typing_expr Var"
+  | _,Var(s) -> (
+      (* try {*)
+        List.assoc s context
+     (* }
+      with {
+        Not_found -> raise Typing_Error
+        }*)
+    )
   | _,App(e1, e2) -> (
       let t1 = typing_expr e1 context
       and t2 = typing_expr e2 context in
       match t1 with
       | TyArrow(a,b) when (t2 = a) -> b
-      | _ -> raise Typing_Error
+      | _ -> raise (Typing_Error "app")
     )
-  | _,Lam(_,_,_) -> failwith "todo typing_expr Lam"
+  | _,Lam(v, Some(ty), e) -> (
+      let b = typing_expr e ((v,ty)::context) in
+      TyArrow(ty,b)
+    )
+  | l,Lam(v, None, e) -> typing_expr (l,Lam(v,Some(TyInt),e)) context
   | _,Pair(e1,e2) -> TyTimes((typing_expr e1 context),(typing_expr e2 context))
-  | _,LetIn(_,_,_) -> failwith "todo typing_expr LetIn"
+  | _,LetIn(v,e1,e2) -> (
+      let t1 = typing_expr e1 context in
+      typing_expr e2 ((v,t1)::context)
+    )
   | _,Fix(e) -> (
       match typing_expr e context with
       | TyArrow(a,b) when a=b -> a
-      | _ -> raise Typing_Error
+      | _ -> raise (Typing_Error "fix")
     )
   | _,Int(_) -> TyInt
   | _,Bool(_) -> TyBool
@@ -32,18 +47,42 @@ let rec typing_expr expr context = match expr with
       | TyBool -> (
           let t1 = typing_expr e1 context
           and t2 = typing_expr e2 context in
-          if t1 = t2 then t1 else (raise Typing_Error)
+          if t1 = t2 then t1 else (raise (Typing_Error "thenelse"))
         )
-      | _ -> raise Typing_Error 
+      | _ -> raise (Typing_Error "if") 
     )
-  | _,Binop(op, e1, e2) -> typing_binop op e1 e2 context
-and typing_binop op e1 e2 context =
+  | l,Binop(op, e1, e2) -> typing_binop op e1 e2 context l
+and typing_binop op e1 e2 context l=
   let t1 = typing_expr e1 context
   and t2 = typing_expr e2 context in
   match op with
-  | Plus | Minus | Times | Div -> if (t1=t2 && t1=TyInt) then t1 else (raise Typing_Error)
-  | Gt -> if (t1=t2 && t1=TyInt) then TyBool else (raise Typing_Error)
-  | Eq -> if t1=t2 then TyBool else (raise Typing_Error)
-  | And | Or -> if (t1=t2 && t1=TyBool) then TyBool else (raise Typing_Error)
+  | Plus | Minus | Times | Div -> if (t1=TyInt && t2=TyInt) then TyInt else (raise (Typing_Error "op Int"))
+  | Gt -> if (t1=TyInt && t2=TyInt) then TyBool else (raise (Typing_Error "op <"))
+  | Eq -> if t1=t2 then TyBool else (
+      match t2 with
+      |TyVar(s) -> raise (Typing_Error s)
+      |TyInt -> raise (Typing_Error "int")
+      |TyTimes(a,b)-> raise(Typing_Error "times")
+      |TyBool -> raise (Typing_Error "bool")
+      |TyArrow(a,b) -> raise(Typing_Error "arrow")
+    )
+  | And | Or -> if (t1=TyBool && t2=TyBool) then TyBool else (raise (Typing_Error "op Bool"))
 
-let rec typing_ast ast = failwith "todo typing ast"
+let rec typing_cmd c context = match c with
+  | l,Let(v,_,e) -> (
+      let t = typing_expr e context in
+      ((l,Let(v,Some(t),e)),(v,t)::context)
+    )
+  | l,LetRec(v,Some(t),e) -> (
+      let tt = typing_expr e ((v,t)::context) in
+      if (tt = t) then
+        ((l,LetRec(v,Some(t),e)),(v,t)::context)
+      else
+        raise (Typing_Error "let rec")
+    )
+  | l,LetRec(v,None,e) -> typing_cmd (l,LetRec(v,Some(TyInt),e)) context
+
+let rec typing_ast ast =
+  let astCont = List.fold_left
+      (fun (a,conte) c -> let (ct,cont)= typing_cmd c conte in ((ct::a),cont))  ([],[]) ast
+  in Ok(fst astCont)
